2f7226727c806758765efdd2031213db0a4a36f7
Ziming Miao <miaoziming@gmail.com>
2013-02-25 17:19:33 +0800

    Migrate yeoman to 1.0beta

--- compass.config.rb
+++ compass.config.rb
@@ -1,6 +1,3 @@
-css_dir = 'temp/styles'
-sass_dir = 'app/styles'
-images_dir = 'app/images'
-http_generated_images_path = '/images';
-http_images_path = '/images';
-javascripts_dir = 'temp/scripts'
+http_generated_images_path = '../images';
+http_images_path = '../images';
+asset_cache_buster :none
