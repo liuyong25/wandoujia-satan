77bfd3c7ae3c92d31b90fce09bec68aa3355efef
Ziming Miao <miaoziming@gmail.com>
2013-04-11 17:53:56 +0800

    Fix bugs

--- app/scripts/nls/messages.js
+++ app/scripts/nls/messages.js
@@ -17,11 +17,13 @@ return {
         BTN_SEND_TIP: 'Send (Enter)',
         BATCH_RECEIVER_TIP: 'To',
 
+        MMS_WARNING: 'This is an MMS. Please read on your phone.',
+
         LABEL_SEND_TO: 'Send to',
 
-        RECEIVER_PLACEHOLDER: 'Add one...',
+        RECEIVER_PLACEHOLDER: 'Add contact...',
 
-        ERROR_SEND_FAILED: 'Send Failed.',
+        ERROR_SEND_FAILED: 'Send failed.',
 
         BLANK_TIP: 'No messages on your phone. C\'mon, make your friends smile.',
         UPGRADE_TIP: 'Update SnapPea to start text messaging from the web.',
@@ -29,8 +31,8 @@ return {
         BTN_LOAD: 'Load more',
         BTN_LOAD_MSG: 'Load earlier messages',
 
-        CONFIRM_DELETE_TITLE: 'Delete messages',
-        CONFIRM_DELETE_CONTENT: 'Delete the selected messages from your phone?',
+        CONFIRM_DELETE_TITLE: 'Delete message(s)',
+        CONFIRM_DELETE_CONTENT: 'Delete the selected message(s) from your phone?',
 
         CONFIRM_DELETE_OK: 'Delete',
         CONFIRM_DELETE_CANCEL: 'Cancel'
