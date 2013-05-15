630b61eb593752e6449a10e52e29eb764bd7b753
Ziming Miao <miaoziming@gmail.com>
2013-05-15 16:19:36 +0800

    Photos major polish done

--- app/scripts/photos/directives/gallery.js
+++ app/scripts/photos/directives/gallery.js
@@ -10,62 +10,67 @@ return {
 scope: true,
 controller: ['$scope', 'GA', 'wdAlert', function($scope, GA, wdAlert) {
     // Selection logic.
-    $scope.selectedPhotos = [];
     $scope.lastSelectedPhoto = null;
 
-    $scope.isSelected = function(photo) {
-        return _.indexOf($scope.selectedPhotos, photo) >= 0;
+    $scope.selectedPhotos = function() {
+        return $scope.photos.filter($scope.isSelected);
     };
-    $scope.toggleSelected = function(selected, photo) {
-        if (selected) {
-            $scope.select(photo);
-            GA('photos:photo:select');
-        }
-        else {
-            $scope.deselect(photo);
-            GA('photos:photo:deselect');
-        }
+    $scope.isSelected = function(photo) {
+        return !!photo.selected;
     };
     $scope.deselectAll = function() {
-        $scope.selectedPhotos = [];
+        $scope.photos.forEach(function(p) {
+            p.selected = false;
+        });
         $scope.lastSelectedPhoto = null;
         GA('photos:toolbar:deselect_all');
     };
-    $scope.select = function(photo) {
-        $scope.selectedPhotos.push(photo);
-    };
-    $scope.batchSelect = function(photo, shiftKey) {
-        if (!shiftKey) {
-            $scope.lastSelectedPhoto = photo;
+    $scope.select = function(photo, shiftKey) {
+        if (photo.selected) {
+            GA('photos:photo:select');
         }
         else {
+            GA('photos:photo:deselect');
+        }
+
+        if (photo.selected && shiftKey) {
             var startIndex = Math.max($scope.photos.indexOf($scope.lastSelectedPhoto), 0);
             var stopIndex = $scope.photos.indexOf(photo);
             $scope.photos.slice(Math.min(startIndex, stopIndex), Math.max(startIndex, stopIndex) + 1).forEach(function(p) {
-                if (!$scope.isSelected(p)) {
-                    $scope.selectedPhotos.push(p);
-                }
+                p.selected = true;
             });
         }
-    };
-    $scope.deselect = function(photo) {
-        $scope.selectedPhotos.splice(_.indexOf($scope.selectedPhotos, photo), 1);
-        if ($scope.lastSelectedPhoto === photo) {
-            $scope.lastSelectedPhoto = null;
+        if (!photo.selected && photo === $scope.lastSelectedPhoto) {
+            var index = $scope.photos.indexOf(photo);
+            var cursor = null;
+            var i, p;
+            for (i = index + 1; i < $scope.photos.length; i += 1) {
+                p = $scope.photos[i];
+                if (p.selected) {
+                    cursor = p;
+                    break;
+                }
+            }
+            if (!cursor) {
+                for (i = index - 1; i >= 0; i -= 1) {
+                    p = $scope.photos[i];
+                    if (p.selected) {
+                        cursor = p;
+                        break;
+                    }
+                }
+            }
+            $scope.lastSelectedPhoto = cursor;
+        }
+        if (photo.selected) {
+            $scope.lastSelectedPhoto = photo;
         }
     };
-    $scope.$on('wdp:photos:remove', function(e, photos) {
-        _.each(photos, function(photo) {
-            $scope.deselect(photo);
-        });
-    });
 
     // Removal logic, delegate real removal to $scope.removePhotos.
     $scope.deleteSelected = function() {
         return confirm().then(function() {
-            $scope.removePhotos($scope.selectedPhotos);
-            $scope.selectedPhotos = [];
-            $scope.lastSelectedPhoto = null;
+            $scope.removePhotos($scope.selectedPhotos());
         });
     };
 
