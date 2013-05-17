89253c6294f93fcb6179b4738f9ed980efd66eb3
Ziming Miao <miaoziming@gmail.com>
2013-05-17 17:19:09 +0800

    Fix sprites issue

--- compass.config.rb
+++ compass.config.rb
@@ -3,7 +3,6 @@ require 'animation'
 css_dir = 'temp/styles'
 sass_dir = 'app/styles'
 images_dir = 'app/images'
-generated_images_dir = 'app/images/sprites'
-http_generated_images_path = '/images/sprites';
-http_images_path = '/images';
+http_images_path = '/images'
+http_generated_images_path = '/images'
 javascripts_dir = 'temp/scripts'
