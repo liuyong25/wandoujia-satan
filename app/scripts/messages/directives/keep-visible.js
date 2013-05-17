a1002b6f38daa3bc06421280507ba78079373d2e
Ziming Miao <miaoziming@gmail.com>
2013-05-17 16:14:49 +0800

    Messages support keyboard shortcut

--- app/scripts/messages/directives/keep-visible.js
+++ app/scripts/messages/directives/keep-visible.js
@@ -8,21 +8,42 @@ return [function() {
 return {
 
 link: function(scope, element) {
+
     scope.$on('wdm:conversations:intoView', function() {
         _.defer(function() {
             var activeElement = element.find('.wdmc-row.highlight');
             if (!activeElement.size()) { return; }
-            var top = activeElement.position().top - element.find('.wdmc-row').first().top;
-            var scrollTop = element.scrollTop();
-            var height = activeElement.outerHeight();
-            var containerHeight = element.height();
 
-            if (top < scrollTop) {
-                element.scrollTop(top);
+            var containerHeight = element.height();
+            var containerTop = element.offset().top;
+            var containerBottom = containerTop + containerHeight;
+
+            var top = activeElement.offset().top - containerTop;
+            var height = activeElement.outerHeight();
+            var bottom = top + height;
+
+            var scrollTop = element.scrollTop();
+
+console.log(containerTop, containerBottom, top, bottom, scrollTop);
+
+            if (top < 0) {
+                element.stop().animate({
+                    scrollTop: scrollTop + top - containerHeight
+                });
             }
-            else if ((top + height) > (scrollTop + containerHeight)) {
-                element.scrollTop(top + height - containerHeight);
+
+            if (bottom > containerHeight) {
+                element.stop().animate({
+                    scrollTop: scrollTop + top
+                });
             }
+
+            // if (top < scroll.Top) {
+            //     element.scrollTop(top);
+            // }
+            // else if ((top + height) > (scrollTop + containerHeight)) {
+            //     element.scrollTop(top + height - containerHeight);
+            // }
         });
     });
 }
